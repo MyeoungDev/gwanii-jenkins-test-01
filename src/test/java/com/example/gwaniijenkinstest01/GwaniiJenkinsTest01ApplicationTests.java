d10027310381fa25d103325c03f2e3091084b14c
gwanii <lung4536@gmail.com>
2023-01-02 21:22:42 +0900

    Initial commit

---
+++ src/test/java/com/example/gwaniijenkinstest01/GwaniiJenkinsTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gwaniijenkinstest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwaniiJenkinsTest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
